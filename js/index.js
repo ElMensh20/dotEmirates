e4d32d95c882ae570b65b27c3a2402a569630572
El Menshawy <mustafaelmensh@stud.fci-cu.edu.eg>
2018-07-10 18:30:36 +0200

    responsive header, header components under develop

---
+++ js/index.js
@@ -0,0 +1,4 @@
+$(".menu_button").click(function(){
+    $(".body").toggle();
+    $("#menu_icon").toggleClass('icon-menu icon-cancel');
+});
---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(".icon-prev").on("click",function(){
+    $(".slider_body li").removeClass("display_none");
+    
+});
+$(".icon-next").on("click",function(){
+
+});
+
+// $(".slider_body li:nth-child("+i+")")
