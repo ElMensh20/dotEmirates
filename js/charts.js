b11ebfcdfb09424d95f9926d582a3cdb780a5604
El Menshawy <mustafaelmensh@stud.fci-cu.edu.eg>
2018-07-11 18:10:54 +0200

    stock not finish, {last screen in header}

---
+++ js/charts.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {  
+    var chart = {
+       type: 'area'
+    };
+    var xAxis = {
+       categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+       tickmarkPlacement: 'on',
+       title: {
+          enabled: false
+       }
+    };
+    var yAxis = {
+       labels: {
+          formatter: function () {
+             return this.value / 1000;
+          }
+       },
+       title: {
+        enabled: false
+     }
+    };
+    // var tooltip = {
+    //    shared: true,
+    //    valueSuffix: ' millions'
+    // };
+    var plotOptions = {
+       area: {
+          stacking: 'normal',
+          lineColor: '#666666',
+          lineWidth: 1,
+
+          marker: {
+             lineWidth: 1,
+             lineColor: '#666666'
+          }
+       }
+    };
+    // var credits = {
+    //    enabled: false
+    // };
+    var series = [
+       {
+          name: 'Asia',
+          data: [502, 635, 809, 947, 1402, 3634, 5268]
+       }, 
+       {
+          name: 'Africa',
+          data: [106, 107, 111, 133, 221, 767, 1766]
+       }, 
+       {
+          name: 'Europe',
+          data: [163, 203, 276, 408, 547, 729, 628]
+       }, 
+       {
+          name: 'America',
+          data: [18, 31, 54, 156, 339, 818, 1201]
+       }, 
+       {
+          name: 'Oceania',
+          data: [2, 2, 2, 6, 13, 30, 46]
+       }
+    ];     
+    var legends= {
+        enabled: false
+    };
+    var json = {};   
+    json.chart = chart; 
+    json.xAxis = xAxis;
+    json.yAxis = yAxis;
+    json.legends=legends;
+    // json.tooltip = tooltip;
+    json.plotOptions = plotOptions;
+    // json.credits = credits;
+    json.series = series;
+    $('#stock_graph_container').highcharts(json);
+ });
--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,27 @@
-$(".icon-prev").on("click",function(){
-    $(".slider_body li").removeClass("display_none");
-    
+counter=120;
+ size= ($(".body_block").children().length-2)*-120;
+ 
+ index=1;
+//Final code *******************************
+$( document ).ready(function() {
+    console.log(size);
+    $(".slider_body .body_block:nth-child(1)").addClass("active_block");
 });
 $(".icon-next").on("click",function(){
-
+    if(counter!=120){
+        $(".slider_body .body_block:nth-child("+index+")").removeClass("active_block");
+        index--;
+        $(".slider_body .body_block:nth-child("+index+")").addClass("active_block");
+        counter+=120;
+        $(".slider_body").css("transform","translate3d("+counter+"px, 0px, 0px)")
+    }
 });
-
-// $(".slider_body li:nth-child("+i+")")
+$(".icon-prev").on("click",function(){
+    if(counter!=size){
+        counter-=120;
+        $(".slider_body .body_block:nth-child("+index+")").removeClass("active_block");
+        index++;
+        $(".slider_body .body_block:nth-child("+index+")").addClass("active_block");
+        $(".slider_body").css("transform","translate3d("+counter+"px, 0px, 0px)")
+    }
+});
