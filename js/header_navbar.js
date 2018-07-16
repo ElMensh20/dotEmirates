dd8c1d583e53504bbff1ef13e9546c951764b056
El Menshawy <mustafaelmensh@stud.fci-cu.edu.eg>
2018-07-16 11:33:35 +0200

    un finished works _ transfare to anoteher pc

--- js/header_navbar.js
+++ js/header_navbar.js
@@ -25,102 +25,100 @@ $("#close_search").click(function(){
     $(".do_search").hide();
 });
 
-
 hrVisible=false;
-$(".gas_price_icon").click(function(){
-    hideAllIconContent(1);
-    $(".gas_price").toggle();
-    hrVisiblity();
-});
-$(".gas_price_icon").hover(function(){
-    $(".gas_price").toggle();
-});
-$(".gas_price").mouseover (function(){
-    $(".gas_price").show();
-});
-$(".gas_price").mouseleave (function(){
-    $(".gas_price").hide();
-});
-
-
-$(".currency_converter_icon").click(function(){
-    hideAllIconContent(2);
-    $(".currency_converter").toggle();
-    hrVisiblity();
-});
-$(".currency_converter_icon").hover(function(){
-    $(".currency_converter").toggle();
-});
-$(".currency_converter").mouseover (function(){
-    $(".currency_converter").show();
-});
-$(".currency_converter").mouseleave (function(){
-    $(".currency_converter").hide();
-});
-
-
-$(".prayer_time_icon").click(function(){
-    hideAllIconContent(3)
-    $(".prayer_time").toggle();
-    hrVisiblity();
-});
-$(".prayer_time_icon").hover(function(){
-    $(".prayer_time").toggle();
-});
-$(".prayer_time").mouseleave (function(){
-    $(".prayer_time").hide();
-});
-$(".prayer_time").mouseover (function(){
-    $(".prayer_time").show();
-});
-
-$(".wheather_icon").click(function(){
-    hideAllIconContent(4);  
-    $(".wheather").toggle();
-    hrVisiblity();
-});
-$(".wheather_icon").hover(function(){
-    $(".wheather").toggle();
-});
-$(".wheather").mouseover (function(){
-    $(".wheather").show();
-});
-$(".wheather").mouseleave (function(){
-    $(".wheather").hide();
-});
-
-
-$(".stock_icon").click(function(){
-    hideAllIconContent(5);  
-    $(".stock").toggle();
-    hrVisiblity();
-});
-$(".stock_icon").hover(function(){
-    $(".stock").toggle();
-});
-$(".stock").mouseover (function(){
-    $(".stock").show();
-});
-$(".stock").mouseleave (function(){
-    $(".stock").hide();
-});
+var viewportWidth;
+$(window).resize(function(){
+    viewportWidth = $(window).width();});
+if (viewportWidth <= 768) {
+    $(".gas_price_icon").click(function(){
+        hideAllIconContent(1);
+        $("mobile_gas_price").toggle();
+        hrVisiblity();
+    });
+    $(".currency_converter_icon").click(function(){
+        hideAllIconContent(2);
+        $(".currency_converter").toggle();
+        hrVisiblity();
+    });
+    $(".prayer_time_icon").click(function(){
+        hideAllIconContent(3)
+        $(".prayer_time").toggle();
+        hrVisiblity();
+    });
+    $(".wheather_icon").click(function(){
+        hideAllIconContent(4);  
+        $(".weather").toggle();
+        hrVisiblity();
+    });
+    $(".stock_icon").click(function(){
+        hideAllIconContent(5);  
+        $(".stock").toggle();
+        hrVisiblity();
+    });
+}else{
+    // $(".gas_price_icon").hover(function(){
+    //     $(".gas_price").toggle();
+    // });
+    // $(".gas_price").mouseover (function(){
+    //     $(".gas_price").show();
+    // });
+    // $(".gas_price").mouseleave (function(){
+    //     $(".gas_price").hide();
+    // });
+    $(".currency_converter_icon").hover(function(){
+        $(".currency_converter").toggle();
+    });
+    $(".currency_converter").mouseover (function(){
+        $(".currency_converter").show();
+    });
+    $(".currency_converter").mouseleave (function(){
+        $(".currency_converter").hide();
+    });
+    $(".prayer_time_icon").hover(function(){
+        $(".prayer_time").toggle();
+    });
+    $(".prayer_time").mouseleave (function(){
+        $(".prayer_time").hide();
+    });
+    $(".prayer_time").mouseover (function(){
+        $(".prayer_time").show();
+    });
+    $(".wheather_icon").hover(function(){
+        $(".weather").toggle();
+    });
+    $(".weather").mouseover (function(){
+        $(".weather").show();
+    });
+    $(".weather").mouseleave (function(){
+        $(".weather").hide();
+    });
+    $(".stock_icon").hover(function(){
+        $(".stock").toggle();
+    });
+    $(".stock").mouseover (function(){
+        $(".stock").show();
+    });
+    $(".stock").mouseleave (function(){
+        $(".stock").hide();
+    });        
+}
 function hideAllIconContent(except){
     switch(except){
         case 1:
             $(".stock").hide();
-            $(".wheather").hide();
+            $(".weather").hide();
             $(".prayer_time").hide();
             $(".currency_converter").hide();
             break;
         case 2:
             $(".stock").hide();
-            $(".wheather").hide();
+            $(".weather").hide();
             $(".prayer_time").hide();
             $(".gas_price").hide();
             break;
         case 3:
             $(".stock").hide();
-            $(".wheather").hide();
+            $(".weather").hide();
             $(".currency_converter").hide();
             $(".gas_price").hide();
             break;
@@ -131,7 +129,7 @@ function hideAllIconContent(except){
             $(".gas_price").hide();
            break;
         case 5:
-            $(".wheather").hide();
+            $(".weather").hide();
             $(".prayer_time").hide();
             $(".currency_converter").hide();
             $(".gas_price").hide();
@@ -148,7 +146,7 @@ function hrVisiblity(){
     }
 }
 function hideAll(){
-    $(".wheather").hide();
+    $(".weather").hide();
     $(".prayer_time").hide();
     $(".currency_converter").hide();
     $(".gas_price").hide();
--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-
+// $(window).resize(function(){
+//     var viewportWidth = $(window).width();
+//     if (viewportWidth < 760) {
+//         $(".view").removeClass("view view-portfolio").addClass("gallery-mobile");
+//     }else{
+        
+//     }
+// });
