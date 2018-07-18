0bab9822adfd7434a5b45ea4a4b291390776de9d
El Menshawy <mustafaelmensh@stud.fci-cu.edu.eg>
2018-07-18 22:48:57 +0200

    gulp new project

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// $(window).resize(function(){
-//     var viewportWidth = $(window).width();
-//     if (viewportWidth < 760) {
-//         $(".view").removeClass("view view-portfolio").addClass("gallery-mobile");
-//     }else{
-        
-//     }
-// });
