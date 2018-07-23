e71312ff7239169f32c39e698ad5f49a1c5bddef
Mustafa El Menshawy <mustafa1elmenshawy@gmail.com>
2018-07-23 15:56:17 +0200

    404 in progress

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,15 +1,10 @@
 counter=0;
 width= $(".slider_item").width();
- size= ($(".slider_item").length-1)*width;
- 
- index=1;
+size= ($(".slider_item").length-1); 
+index=0;
 //Final code *******************************
 $( document ).ready(function() {
-    console.log(size);
-    console.log(width);
-    
-    console.log($(".slider_item").length);
-    
+    $(".works .work_slider .slider_body .work:nth-child(2)").addClass("active");
     $(".slider_body .slider_item:nth-child(1)").addClass("focused");
 });
 $(".icon-prev").on("click",function(){
@@ -19,7 +14,6 @@ $(".icon-prev").on("click",function(){
         $(".slider_body .slider_item:nth-child("+index+")").addClass("focused");
         counter+=width;
         $(".slider_body").css("transform","translate3d("+counter+"px, 0px, 0px)");
-        console.log(counter);
     }
 });
 $(".icon-next").on("click",function(){
@@ -29,6 +23,46 @@ $(".icon-next").on("click",function(){
         index++;
         $(".slider_body .slider_item:nth-child("+index+")").addClass("focused");
         $(".slider_body").css("transform","translate3d("+counter+"px, 0px, 0px)");
-        console.log(counter);
     }
+});
+
+workIndex=2;
+workCounter=0;
+workWidth=$(".works .work_slider .slider_body .work").width();
+worksliderSize=($(".works .work_slider .slider_body .work").length-1)*workWidth;
+$(".icon-left").on("click",function(){
+    if( workCounter!=(workWidth)){
+        $(".works .work_slider .slider_body .work").removeClass("active");
+        workIndex--;
+        $(".works .work_slider .slider_body .work:nth-child("+workIndex+")").addClass("active");
+        workCounter+=workWidth;
+        $(".works .work_slider .slider_body").css("transform","translate3d("+workCounter+"px, 0px, 0px)");
+    }
+});
+$(".icon-right").on("click",function(){
+    if(workCounter!=-(worksliderSize-workWidth)){
+        workCounter-=workWidth;
+        $(".works .work_slider .slider_body .work").removeClass("active");
+        workIndex++;
+        $(".works .work_slider .slider_body .work:nth-child("+workIndex+")").addClass("active");
+        $(".works .work_slider .slider_body").css("transform","translate3d("+workCounter+"px, 0px, 0px)");    }
+});
+
+wordIndex=1;
+wordCounter=0;
+wordWidth=$(".his_words .inner .words_slider .slider_body .word").width();
+wordsliderSize=($(".his_words .inner .words_slider .slider_body .word").length-1)*wordWidth;
+$(".his_words .inner .slider_footer .icon-left").on("click",function(){
+    if( wordCounter!=0){
+        wordIndex--;
+        wordCounter+=wordWidth;
+        $(".his_words .inner .words_slider .slider_body").css("transform","translate3d("+wordCounter+"px, 0px, 0px)");
+    }
+});
+$(".his_words .inner .slider_footer .icon-right").on("click",function(){
+    if(wordCounter!=-(wordsliderSize)){
+        wordCounter-=wordWidth;
+        wordIndex++;
+        $(".his_words .inner .words_slider .slider_body").css("transform","translate3d("+wordCounter+"px, 0px, 0px)");  
+    } 
 });
